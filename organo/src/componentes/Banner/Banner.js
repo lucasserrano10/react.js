3cab93abd7926d3a1c6b58b1b9132281b2e90110
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-09-02 16:13:35 -0300

    fix: 'banner do projeto'

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="bannerPrincipal"/>
+        </header>
+    )
+}
+
+export default Banner
