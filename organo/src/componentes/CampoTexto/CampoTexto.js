a3e3a6a11bd13a6ee5e7f2a9c2443e4c3d764fe3
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-09-12 22:25:27 -0300

    fix: 'atualizando campo texto e criando btn'

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,22 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...` 
+
+    let valor = 'Guilherme Silveira'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return(
         <div className="campo-texto">
-            <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <label>
+                {props.label}
+            </label>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholderModificada}/>
         </div>
     )
 }
--- organo/src/componentes/Formulario/Formulario.js
+++ organo/src/componentes/Formulario/Formulario.js
@@ -13,12 +13,17 @@ const Formulario = () => {
         'Treinadores'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido !')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do Atleta</h2>
-                <CampoTexto label="Nome" placeholder="Digite Seu Nome..." />
-                <CampoTexto label="Esporte" placeholder="Digite Seu Esporte..." />
+                <CampoTexto obrigatorio = {true} label="Nome" placeholder="Digite Seu Nome..." />
+                <CampoTexto obrigatorio = {true}  label="Esporte" placeholder="Digite Seu Esporte..." />
                 <CampoTexto label="Imagem" placeholder="Digite o Endereço da Imagem"/>
                 <ListaSuspensa label = "Time"  itens = {times} />
                 <Botao>
